049696e7842d1c6bcd43228fb76ea346616da632
Nick <35513415+nickolasgiese@users.noreply.github.com>
2023-04-18 01:09:33 -0400

    Update server.js

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
-    url: env('URL', ''),
+    url: env('URL', 'https://deft-gnome-afe72b.netlify.app'),
     admin: {
       auth: {
         secret: env('ADMIN_JWT_SECRET', 'some_secret_key'),
